323b6cfe29e47c653a50f775db9dce51b6050b37
Terry Tsai <ttsai0509@gmail.com>
2016-10-11 19:27:38 -0500

    Root path forwards to index.html

--- src/main/java/yatbd/StaticConfig.java
+++ src/main/java/yatbd/StaticConfig.java
@@ -2,10 +2,16 @@ package yatbd;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class StaticConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
