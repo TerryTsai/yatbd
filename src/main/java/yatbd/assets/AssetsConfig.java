9bced89dc38d72c44ef3f25cea5c6120b2002bb8
Terry Tsai <ttsai0509@gmail.com>
2016-10-11 19:37:39 -0500

    Started organizing

--- src/main/java/yatbd/assets/AssetsConfig.java
+++ src/main/java/yatbd/assets/AssetsConfig.java
@@ -1,4 +1,4 @@
-package yatbd;
+package yatbd.assets;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class StaticConfig extends WebMvcConfigurerAdapter {
+public class AssetsConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
--- src/main/java/yatbd/hello/HelloController.java
+++ src/main/java/yatbd/hello/HelloController.java
@@ -1,18 +1,16 @@
-package yatbd;
+package yatbd.hello;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
 @RestController
-@RequestMapping("/test")
-public class TestController {
+@RequestMapping("/hello")
+public class HelloController {
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String test() {
-        return "Hello World";
+        return "Hello.";
     }
 
 }
